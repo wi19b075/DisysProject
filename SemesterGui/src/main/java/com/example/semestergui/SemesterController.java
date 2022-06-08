fe4b03d4a0c98e0b7a1dc088f8c217184a39f9a5
wi19b075 <71829917+wi19b075@users.noreply.github.com>
2022-06-08 11:11:21 +0200

    Add files via upload

--- SemesterGui/src/main/java/com/example/semestergui/SemesterController.java
+++ SemesterGui/src/main/java/com/example/semestergui/SemesterController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SemesterController {
@@ -12,7 +13,11 @@ public class SemesterController {
     private SemesterApplication semesterApplication;
 
     @FXML
-    private Label welcomeText;
+    private static final String API = "http://localhost:8080/chargings";
+
+
+    @FXML
+    private TextField idNumber;
 
     @FXML
     private Button quit;
@@ -23,18 +28,14 @@ public class SemesterController {
 
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-    @FXML
     public void handleCloseButtonAction(ActionEvent event) {
         Stage stage = (Stage) quit.getScene().getWindow();
         stage.close();
     }
 
     public void generateInvoice() {
-
+        String id = idNumber.getText();
+        // id nun an Datenbank schicken
     }
 
     @FXML
--- SemesterGui/src/main/java/module-info.java
+++ SemesterGui/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.semestergui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+
 
 
     opens com.example.semestergui to javafx.fxml;
