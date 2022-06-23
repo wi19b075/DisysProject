d9e86cb15d230e6a91a45a5c3f53924eeb23a88a
Nadiia Goncharenko <83373973+NadiiaGoncharenko@users.noreply.github.com>
2022-06-23 23:43:33 +0200

    dummy Daten hinzugefügt

--- SemesterDatenbank/src/main/java/Main.java
+++ SemesterDatenbank/src/main/java/Main.java
@@ -22,6 +22,45 @@ public class Main {
         preparedStatement.setString(3, "Adresse2" );
         preparedStatement.setString(4, "City1" );
         preparedStatement.setInt(5, 1234 );
+        
+        //dummy Data
+        
+        preparedStatement.setInt(1, 1239 );
+        preparedStatement.setString(2, "John Doe" );
+        preparedStatement.setString(3, "Derybasivska 3" );
+        preparedStatement.setString(4, "Odessa" );
+        preparedStatement.setInt(5, 1100 );
+
+        preparedStatement.setInt(1, 1537 );
+        preparedStatement.setString(2, "Julia Fisher" );
+        preparedStatement.setString(3, "vul.Vilna 68" );
+        preparedStatement.setString(4, "Kyiv" );
+        preparedStatement.setInt(5, 03164 );
+
+        preparedStatement.setInt(1, 1722 );
+        preparedStatement.setString(2, "Rza Gurbanov" );
+        preparedStatement.setString(3, "Johannesgasse 4a" );
+        preparedStatement.setString(4, "Wien" );
+        preparedStatement.setInt(5, 1060 );
+
+        preparedStatement.setInt(1, 1352 );
+        preparedStatement.setString(2, "Michi Att" );
+        preparedStatement.setString(3, "Rotenhoffgasse 11" );
+        preparedStatement.setString(4, "Wien" );
+        preparedStatement.setInt(5, 1020 );
+
+        preparedStatement.setInt(1, 2643 );
+        preparedStatement.setString(2, "Olga Andrienko" );
+        preparedStatement.setString(3, "vul.Sichovych striltziv 124" );
+        preparedStatement.setString(4, "Vinnytsia" );
+        preparedStatement.setInt(5, 03352 );
+
+        preparedStatement.setInt(1, 3622 );
+        preparedStatement.setString(2, "Viktoria Moll" );
+        preparedStatement.setString(3, "prov.Tchervonozavodsky 2/13" );
+        preparedStatement.setString(4, "Kyiv" );
+        preparedStatement.setInt(5, 03162 );
+
 
         preparedStatement.execute();
 
